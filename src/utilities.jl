08bdd98c28279f13576675cbc9e244bb6bf825d5
Johnny Chen <johnnychen94@hotmail.com>
2021-09-07 20:23:53 +0800

    optimize block matching memory allocation

--- src/utilities.jl
+++ src/utilities.jl
@@ -39,6 +39,33 @@ function mean2!(out::AbstractVector, m::AbstractMatrix)
     end
     return out
 end
+
+# Copy from BlockMatching with extra `dist_buffer` input to reuse the memory
+function multi_match(S::FullSearch, ref, frame, p::CartesianIndex, dist_buffer; num_patches)
+    rₚ = S.patch_radius
+    R_frame = CartesianIndices(frame)
+    R_frame = first(R_frame) + rₚ : last(R_frame) - rₚ
+    p in R_frame || throw(ArgumentError("Boundary unsupported now: pixel `p = $p` should be within $(first(R_frame)):$(last(R_frame))."))
+
+    R_ref = CartesianIndices(ref)
+    R_ref = first(R_ref) + rₚ : last(R_ref) - rₚ
+    q_start = max(first(R_ref), p - S.search_radius)
+    q_stop = min(last(R_ref), p + S.search_radius)
+    candidates = q_start:S.search_stride:q_stop
+
+    patch_p = frame[p - rₚ:p + rₚ]
+    @assert length(dist_buffer) >= length(candidates)
+    dist = dist_buffer
+    @inbounds for i in 1:length(candidates)
+        q = candidates[i]
+        patch_q = @view ref[q - rₚ:q + rₚ]
+        dist[i] = S.f(patch_p, patch_q)
+    end
+
+    indices = partialsortperm(view(dist, 1:length(candidates)), 1:num_patches)
+    candidates[indices]
+end
+
 # Threads helper
 
 function with_blas_threads(f, num_threads)
